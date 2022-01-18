1dfb2de91de4f1f08fde9d396578638249fb88b4
Serj3000 <serjio230@ukr.net>
2022-01-18 10:30:52 +0200

    1) migrate

---
+++ database/migrations/2022_01_18_080321_create_categories_table.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at')->nullable();
+        });
+
+        Schema::create('sub_categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('cat_id');
+            $table->string('name');
+            $table->string('slug');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at')->nullable();
+        });
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('sub_cat_id');
+            $table->string('name');
+            $table->text('description');
+            $table->string('slug');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at')->nullable();
+        });
+
+        Schema::table('sub_categories', function (Blueprint $table) {
+            $table->foreign('cat_id')
+                ->references('id')->on('categories')
+                ->onDelete('cascade');
+        });
+
+        Schema::table('products', function (Blueprint $table) {
+            $table->foreign('sub_cat_id')
+                ->references('id')->on('sub_categories')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('sub_categories');
+        Schema::dropIfExists('products');
+    }
+}
